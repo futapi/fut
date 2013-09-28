4b3e3cddef58631dd9244e30452eed45e9bb4a7b
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-09-29 01:35:38 +0200

    add exceptions

---
+++ fut14/exceptions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+fut14.exceptions
+~~~~~~~~~~~~~~~~~~~
+
+This module contains the set of fut14's exceptions.
+
+"""
+
+
+class Fut14Error(RuntimeError):
+    """There was an ambiguous exception that occurred while handling
+    your request."""
