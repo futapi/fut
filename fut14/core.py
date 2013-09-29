0693a606bd3a8ab2c88706b10ca71fabbb342747
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-09-29 22:28:56 +0200

    core: using website instead of api(?) & add authorizatation on fut urls: update & cleanup

--- fut14/core.py
+++ fut14/core.py
@@ -9,62 +9,85 @@ This module implements the fut14's basic methods.
 """
 
 import requests
-import xmltodict
+import re
+import simplejson as json  # TODO: try json if simplejson is not avaible
 #from time import time
 
 from .config import headers
 from .urls import urls
-from .exceptions import Fut14Error
-#from .EAHashingAlgorithm import EAHashingAlgorithm
+#from .exceptions import Fut14Error
+from .EAHashingAlgorithm import EAHashingAlgorithm
 
 
 class Core(object):
     def __init__(self, email, passwd, secret_answer):
         self.email = email
         self.passwd = passwd
-        #eahashor = EAHashingAlgorithm()
-        #self.secret_answer_hash = eahashor.EAHash(secret_answer)
-        self.secret_answer_hash = None
+        eahashor = EAHashingAlgorithm()
+        self.secret_answer_hash = eahashor.EAHash(secret_answer)
         self.login(self.email, self.passwd, self.secret_answer_hash)
 
     def login(self, email, passwd, secret_answer_hash):
         """Just log in."""
-        self.r = requests.Session()
-        self.r.headers = headers
+        self.r = requests.Session()  # init/reset requests session object
+        self.r.headers = headers  # i'm chrome browser now ;-)
+
+        # === login
+        urls['login'] = self.r.get(urls['fut_home']).url
         self.r.headers['Referer'] = urls['main_site']
-        data = {'email': email, 'password': passwd, 'overlay-stay-signed': 'ON'}
-        rc = xmltodict.parse(self.r.post(urls['login'], data=data).content)
+        data = {'email': email, 'password': passwd, '_rememberMe': 'on', 'rememberMe': 'on', '_eventId': 'submit', 'facebookAuth': ''}
+        rc = self.r.post(urls['login'], data=data).content
+        # TODO: catch invalid data exception
+        #self.nucleus_id = re.search('userid : "([0-9]+)"', rc).group(1)  # we'll get it later
 
-        if 'authenticate' in rc and rc['authenticate']['success'] == '0':
-            raise Fut14Error('Invalid email or password.')
-
-        self.player_id = rc['login']['player']['id']
-        self.nucleus_id = rc['login']['player']['nucleusId']
-        self.persona_id = rc['login']['player']['preferredPersona']['id']
-        self.persona_gamertag = rc['login']['player']['preferredPersona']['gamertag']
-        self.persona_platform = rc['login']['player']['preferredPersona']['platform']
+        # === lanuch futweb
+        rc = self.r.get(urls['futweb']).content
+        self.nucleus_id = re.search("var EASW_ID = '([0-9]+)';", rc).group(1)
+        urls['fut_base'] = re.search("var BASE_FUT_URL = '(https://.+?)';", rc).group(1)
 
         # prepare headers for ut operations
         self.r.headers['Content-Type'] = 'application/json'
         self.r.headers['Easw-Session-Data-Nucleus-Id'] = self.nucleus_id
         self.r.headers['X-UT-Embed-Error'] = 'true'
         self.r.headers['X-Requested-With'] = 'XMLHttpRequest'
+        self.r.headers['X-UT-Route'] = urls['fut_host']
+        #self.r.headers['X-UT-PHISHING-TOKEN'] = ?
+        #self.r.headers['X-HTTP-Method-Override'] = ?
         # TODO: dynamic create urls based on shards
+
+        # acc info
+        rc = self.r.get(urls['acc_info']).json()
+        self.persona_id = rc['userAccountInfo']['personas'][0]['personaId']
+        self.persona_name = rc['userAccountInfo']['personas'][0]['personaName']
+        self.clubs = [i for i in rc['userAccountInfo']['personas'][0]['userClubList']]
+        # sort clubs by lastAccessTime (latest firts)
+        self.clubs.sort(key=lambda i: i['lastAccessTime'], reverse=True)
+
+        # authorization
+        data = {'isReadOnly': False,
+                'sku': 'FUT14WEB',
+                'clientVersion': 1,
+                'nuc': self.nucleus_id,
+                'nucleusPersonaId': self.persona_id,
+                'nucleusPersonaDisplayName': self.persona_name,
+                'nucleusPersonaPlatform': 'pc',  # TODO: multiplatform
+                'locale': 'en-GB',
+                'method': 'authcode',
+                'priorityLevel': 4,
+                'identification': {'AuthCode': ''}}
+        rc = self.r.post(urls['fut_auth'], data=json.dumps(data)).json()
+        #urls['fut_host'] = '{0}://{1}'.format(rc['protocol']+rc['ipPort'])
+        self.sid = rc['sid']
+        self.r.headers['X-UT-SID'] = self.sid
+
+        # validate (secret question)
+        data = {'answer': self.secret_answer_hash}
+        print self.r.post(urls['fut_validate'], data=json.dumps(data)).content
+
+        #self.r.headers['Referer'] = 'http://www.easports.com/iframe/fut/bundles/futweb/web/flash/FifaUltimateTeam.swf'
 
 #    def shards(self):
 #        """Returns shards info."""
-#        self.r.headers['X-UT-Route'] = urls['home']
+#        self.r.headers['X-UT-Route'] = urls['fut_base']
 #        return self.r.get(urls['shards']).json()
-
-    def getClubs(self):
-        """Returns all clubs info."""
-        # ony clubs with year 2014 (year==fifa version) ??
-        self.r.headers['X-UT-Route'] = urls['home_pc']
-        rc = self.r.get(urls['acc_info']).json()
-
-        # list of clubes (default sorted by id?)
-        clubs = [i for i in rc['userAccountInfo']['personas'][0]['userClubList']]
-        # sorted by lastAccessTime
-        clubs.sort(key=lambda i: i['lastAccessTime'], reverse=True)
-
-        return clubs
+#        # self.r.headers['X-UT-Route'] = urls['fut_pc']
--- fut14/urls.py
+++ fut14/urls.py
@@ -3,13 +3,13 @@
 from time import time
 
 urls = {
-
     'main_site':    'https://www.easports.com',
-    'login':        'https://www.easports.com/services/authenticate/login',
-    'home':         'https://utas.fut.ea.com',
-    'home_pc':      'https://utas.s2.fut.ea.com:443',
+    'futweb':       'http://www.easports.com/iframe/fut/?locale=en_GB&baseShowoffUrl=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team',
+    'fut_home':     'http://www.easports.com/uk/fifa/football-club/ultimate-team',
+    'fut_host':     'https://utas.s2.fut.ea.com:443',  # PC - different on other platforms
+    'fut_auth':     'http://www.easports.com/iframe/fut/p/ut/auth',
+    'fut_validate': 'http://www.easports.com/iframe/fut/p/ut/game/fifa14/phishing/validate',
+
     'shards':       'http://www.easports.com/iframe/fut/p/ut/shards?_=%s' % time(),
     'acc_info':     'http://www.easports.com/iframe/fut/p/ut/game/fifa14/user/accountinfo?_=%s' % time(),
-    #'home_ut':     '',
-
 }
