239a40df7d0b15a0e9936ecb5412d51f3e5f40ec
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-10-10 15:50:31 +0200

    urls: en-GB -> en_US

--- fut14/urls.py
+++ fut14/urls.py
@@ -6,7 +6,7 @@ from time import time
 
 urls = {
     'main_site':    'https://www.easports.com',
-    'futweb':       'http://www.easports.com/iframe/fut/?locale=en_GB&baseShowoffUrl=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team',
+    'futweb':       'http://www.easports.com/iframe/fut/?locale=en_US&baseShowoffUrl=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team',
     'fut_config':   'http://www.easports.com/iframe/fut/bundles/futweb/web/flash/xml/site_config.xml',
     'fut_home':     'http://www.easports.com/uk/fifa/football-club/ultimate-team',
     'fut_host':     'https://utas.s2.fut.ea.com:443',  # PC - different on other platforms
