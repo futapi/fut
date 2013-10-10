8d7c3b53e0f910c10be76ac240673b685f189867
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-10-10 15:45:13 +0200

    config: update headers

--- fut14/config.py
+++ fut14/config.py
@@ -1,11 +1,10 @@
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31',
-    'Accept': 'text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1',
-    'Accept-Encoding': 'deflate, gzip, x-gzip, identity, *;q=0',
-    'Accept-Language': 'en;q=0.8',
-    'Accept-Charset': 'utf-8, iso-8859-1, utf-16, *;q=0.1',
-    'Connection': 'Keep-Alive, TE',
-    'Keep-Alive': '300',
-    'TE': 'deflate, gzip, chunked, identity, trailers',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip,deflate,sdch',
+    'Accept-Language': 'en-US,en;q=0.8',
+    #'Accept-Charset': 'utf-8, iso-8859-1, utf-16, *;q=0.1',
+    'Connection': 'keep-alive',
+    #'Keep-Alive': '300',
     'DNT': '1',
 }
