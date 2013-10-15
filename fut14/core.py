2332c4a23ef200236249bd1ba9015f119e71e691
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-10-15 11:58:06 +0200

    move urls update method to urls & cleanup

--- fut14/core.py
+++ fut14/core.py
@@ -9,7 +9,6 @@ This module implements the fut14's basic methods.
 """
 
 import requests
-import xmltodict
 import re
 try:
     import simplejson as json
@@ -40,27 +39,7 @@ class Core(object):
         self.email = email
         self.passwd = passwd
         self.secret_answer_hash = EAHashingAlgorithm().EAHash(secret_answer)
-        self.urls = urls
         self.login(self.email, self.passwd, self.secret_answer_hash)
-
-    def getUrls(self):
-        """Gets services urls."""
-        urls_fut = {}
-        rc = xmltodict.parse(requests.get(self.urls['fut_config']).content)
-        services = rc['main']['services']['prod']
-        path = '{0}{1}game/fifa14/'.format(
-            self.urls['fut_host'], rc['main']['directHttpServiceDestination']
-        )
-        for i in services:
-            if i == 'authentication':
-                urls_fut[i] = '{0}/iframe/fut{1}{2}'.format(
-                    self.urls['main_site'].replace('https', 'http'),  # it's not working with ssl...
-                    rc['main']['httpServiceDestination'],
-                    services[i]
-                )
-            else:
-                urls_fut[i] = '{0}{1}'.format(path, services[i])
-        return urls_fut
 
     def login(self, email, passwd, secret_answer_hash):
         """Just log in."""
@@ -68,26 +47,22 @@ class Core(object):
         self.r = requests.Session()  # init/reset requests session object
         self.r.headers = headers  # i'm chrome browser now ;-)
 
-        # === update urls
-        self.urls = urls
-        self.urls['fut'] = self.getUrls()
-
         # === login
-        self.urls['login'] = self.r.get(self.urls['fut_home']).url
-        self.r.headers['Referer'] = self.urls['main_site']  # prepare headers
+        urls['login'] = self.r.get(urls['fut_home']).url
+        self.r.headers['Referer'] = urls['main_site']  # prepare headers
         data = {'email': email, 'password': passwd, '_rememberMe': 'on', 'rememberMe': 'on', '_eventId': 'submit', 'facebookAuth': ''}
-        rc = self.r.post(self.urls['login'], data=data).content
+        rc = self.r.post(urls['login'], data=data).content
         # TODO: catch invalid data exception
         #self.nucleus_id = re.search('userid : "([0-9]+)"', rc).group(1)  # we'll get it later
 
         # === lanuch futweb
-        self.r.headers['Referer'] = self.urls['fut_home']  # prepare headers
-        rc = self.r.get(self.urls['futweb']).content
+        self.r.headers['Referer'] = urls['fut_home']  # prepare headers
+        rc = self.r.get(urls['futweb']).content
         if 'EASW_ID' not in rc:
             raise Fut14Error('Invalid email or password.')
         self.nucleus_id = re.search("var EASW_ID = '([0-9]+)';", rc).group(1)
-        #self.urls['fut_base'] = re.search("var BASE_FUT_URL = '(https://.+?)';", rc).group(1)
-        #self.urls['fut_home'] = re.search("var GUEST_APP_URI = '(http://.+?)';", rc).group(1)
+        #urls['fut_base'] = re.search("var BASE_FUT_URL = '(https://.+?)';", rc).group(1)
+        #urls['fut_home'] = re.search("var GUEST_APP_URI = '(http://.+?)';", rc).group(1)
 
         # acc info
         self.r.headers.update({  # prepare headers
@@ -96,10 +71,10 @@ class Core(object):
             'Easw-Session-Data-Nucleus-Id': self.nucleus_id,
             'X-UT-Embed-Error': 'true',
             'X-Requested-With': 'XMLHttpRequest',
-            'X-UT-Route': self.urls['fut_host'],
-            'Referer': self.urls['futweb'],
+            'X-UT-Route': urls['fut_host'],
+            'Referer': urls['futweb'],
         })
-        rc = self.r.get(self.urls['acc_info']).json()
+        rc = self.r.get(urls['acc_info']).json()
         self.persona_id = rc['userAccountInfo']['personas'][0]['personaId']
         self.persona_name = rc['userAccountInfo']['personas'][0]['personaName']
         self.clubs = [i for i in rc['userAccountInfo']['personas'][0]['userClubList']]
@@ -122,35 +97,25 @@ class Core(object):
                 'method': 'authcode',
                 'priorityLevel': 4,
                 'identification': {'AuthCode': ''}}
-        rc = self.r.post(self.urls['fut']['authentication'], data=json.dumps(data)).json()
-        # {u'debug': u'', u'reason': u'multiple session', u'code': u'401', u'string': u'Unauthorized (ut)'}
-        #self.urls['fut_host'] = '{0}://{1}'.format(rc['protocol']+rc['ipPort'])
+        rc = self.r.post(urls['fut']['authentication'], data=json.dumps(data)).json()
+        #urls['fut_host'] = '{0}://{1}'.format(rc['protocol']+rc['ipPort'])
         self.sid = rc['sid']
         self.r.headers['X-UT-SID'] = self.sid
 
         # validate (secret question)
         self.r.headers['Accept'] = 'text/json'  # prepare headers
         del self.r.headers['Origin']
-        rc = self.r.get(self.urls['fut_question']).json()
-        # {"question":1,"attempts":5,"recoverAttempts":20}
-        # {"debug":"Already answered question.","string":"Already answered question","reason":"Already answered question.","token":"0000000000000000000","code":"483"}
+        rc = self.r.get(urls['fut_question']).json()
         if rc.get('string') == 'Already answered question.':
             self.token = rc['string']
         else:
             # answer question
             data = {'answer': self.secret_answer_hash}
             self.r.headers['Content-Type'] = 'application/x-www-form-urlencoded'  # requests bug?
-            rc = self.r.post(self.urls['fut_validate'], data=data).json()
-            #{"debug":"Answer is correct.","string":"OK","reason":"Answer is correct.","token":"0000000000000000000","code":"200"}
+            rc = self.r.post(urls['fut_validate'], data=data).json()
             self.r.headers['Content-Type'] = 'application/json'
             self.token = rc['token']
         self.r.headers['X-UT-PHISHING-TOKEN'] = self.token
-
-        #print self.r.get('http://www.easports.com/iframe/fut/p/ut/game/fifa14/item/resource').content
-        #print self.r.get('http://www.easports.com/iframe/fut/p/ut/game/fifa14/item').content
-        #print self.r.get('http://www.easports.com/iframe/fut/p/ut/game/fifa14/item/resource/1615614739').content
-
-        # http://cdn.content.easports.com/fifa/fltOnlineAssets/C74DDF38-0B11-49b0-B199-2E2A11D1CC13/2014/fut/items/web/5002003.json
 
         # prepare headers for ut operations
         del self.r.headers['Easw-Session-Data-Nucleus-Id']
@@ -167,9 +132,9 @@ class Core(object):
 #    def shards(self):
 #        """Returns shards info."""
 #        # TODO: headers
-#        self.r.headers['X-UT-Route'] = self.urls['fut_base']
-#        return self.r.get(self.urls['shards']).json()
-#        # self.r.headers['X-UT-Route'] = self.urls['fut_pc']
+#        self.r.headers['X-UT-Route'] = urls['fut_base']
+#        return self.r.get(urls['shards']).json()
+#        # self.r.headers['X-UT-Route'] = urls['fut_pc']
 
     def searchAuctions(self, ctype, level=None, category=None, min_price=None,
                        max_price=None, min_buy=None, max_buy=None, league=None,
@@ -196,7 +161,7 @@ class Core(object):
         if nationality: params['nat'] = nationality
         if playStyle:   params['playStyle'] = playStyle
 
-        rc = self.r.get(self.urls['fut']['SearchAuctions'], params=params).json()
+        rc = self.r.get(urls['fut']['SearchAuctions'], params=params).json()
         self.credits = rc['credits']
 
         items = []
@@ -233,18 +198,15 @@ class Core(object):
 
     def bid(self, trade_id, bid):
         """Make a bid."""
-        rc = self.r.get(self.urls['fut']['PostBid'], params={'tradeIds': trade_id}).json()
-        if 'auctionInfo' not in rc: print rc  # DEBUG
+        rc = self.r.get(urls['fut']['PostBid'], params={'tradeIds': trade_id}).json()
         if rc['auctionInfo'][0]['currentBid'] < bid and self.credits >= bid:
             data = {'bid': bid}
-            url = '{0}/{1}/bid'.format(self.urls['fut']['PostBid'], trade_id)
+            url = '{0}/{1}/bid'.format(urls['fut']['PostBid'], trade_id)
 
             self.r.headers['X-HTTP-Method-Override'] = 'PUT'  # prepare headers
             rc = self.r.post(url, data=json.dumps(data)).json()
             self.r.headers['X-HTTP-Method-Override'] = 'GET'  # restore headers default
 
-        if 'credits' not in rc: print rc  # DEBUG
-        # {u'debug': u'', u'reason': u'', u'code': u'401', u'string': u'Unauthorized (ut)'}  # DEBUG
         self.credits = rc['credits']  # update credits info
         if rc['auctionInfo'][0]['bidState'] == 'highest':
             return True
@@ -286,7 +248,6 @@ class Core(object):
                 'currentBid':     i['currentBid'],
                 'expires':        i['expires'],  # seconds left
             })
-        if len(items) == 0: print rc  # DEBUG
 
         return items
 
@@ -306,8 +267,6 @@ class Core(object):
 
     def watchlist_delete(self, trade_id):
         """Removes card from watchlist."""
-        # https://utas.s2.fut.ea.com/ut/game/fifa14/watchlist?tradeId=136826808001
-        # method DELETE
         params = {'tradeId': trade_id}
 
         self.r.headers['X-HTTP-Method-Override'] = 'DELETE'  # prepare headers
@@ -318,8 +277,6 @@ class Core(object):
 
     def tradepile_delete(self, trade_id):
         """Removes card from tradepile."""
-        # https://utas.s2.fut.ea.com/ut/game/fifa14/trade/137005224869
-        # method DELETE
         url = '{}/{}'.format(urls['fut']['TradeInfo'], trade_id)
 
         self.r.headers['X-HTTP-Method-Override'] = 'DELETE'  # prepare headers
@@ -335,7 +292,7 @@ class Core(object):
         params = {'tradeId': trade_id}
 
         self.r.headers['X-HTTP-Method-Override'] = 'PUT'  # prepare headers
-        print self.r.post(urls['fut']['WatchList'], params=params, data=json.dumps(data)).content
+        self.r.post(urls['fut']['WatchList'], params=params, data=json.dumps(data))  # returns nothing
         self.r.headers['X-HTTP-Method-Override'] = 'GET'  # restore headers default
 
         return True
--- fut14/urls.py
+++ fut14/urls.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
 
+import requests
+import xmltodict
 from time import time
 
 # TODO: add timestamp dynamic (just right before every request)
 
-urls = {
+def updateUrls(urls):
+        """Gets services urls."""
+        rc = xmltodict.parse(requests.get(urls['fut_config']).content)
+        services = rc['main']['services']['prod']
+        path = '{0}{1}game/fifa14/'.format(
+            urls['fut_host'], rc['main']['directHttpServiceDestination']
+        )
+        for i in services:
+            if i == 'authentication':
+                urls['fut'][i] = '{0}/iframe/fut{1}{2}'.format(
+                    urls['main_site'].replace('https', 'http'),  # it's not working with ssl...
+                    rc['main']['httpServiceDestination'],
+                    services[i]
+                )
+            else:
+                urls['fut'][i] = '{0}{1}'.format(path, services[i])
+        return urls
+
+urls = updateUrls({
     'main_site':    'https://www.easports.com',
     'futweb':       'http://www.easports.com/iframe/fut/?locale=en_US&baseShowoffUrl=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team',
     'fut_config':   'http://www.easports.com/iframe/fut/bundles/futweb/web/flash/xml/site_config.xml',
@@ -16,4 +36,4 @@ urls = {
 
     'shards':       'http://www.easports.com/iframe/fut/p/ut/shards?_=%s' % time(),
     'acc_info':     'http://www.easports.com/iframe/fut/p/ut/game/fifa14/user/accountinfo?_=%s' % time(),
-}
+})
