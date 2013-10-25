f953cc90ecc604d54fdd4d438b8aa39edbe5468c
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-10-25 21:09:14 +0200

    setup: python version classifiers

--- setup.py
+++ setup.py
@@ -44,11 +44,11 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3',
         #'Programming Language :: Python :: 3.0',  # not tested
         #'Programming Language :: Python :: 3.1',  # not tested
-        #'Programming Language :: Python :: 3.2',  # not tested
-        #'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         #'Programming Language :: Python :: Implementation :: CPython',  # not tested
         #'Programming Language :: Python :: Implementation :: IronPython',  # not tested
         'Programming Language :: Python :: Implementation :: PyPy',
