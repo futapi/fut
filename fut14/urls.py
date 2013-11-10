aa9ab1c14ac9cf5c3677014d171428d440716bf4
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-11-10 15:11:04 +0100

    urls: remove unused keepalive

--- fut14/urls.py
+++ fut14/urls.py
@@ -33,5 +33,4 @@ urls = updateUrls({
     'shards':       'http://www.easports.com/iframe/fut/p/ut/shards?_=%s' % time(),
     'acc_info':     'http://www.easports.com/iframe/fut/p/ut/game/fifa14/user/accountinfo?_=%s' % time(),
     'card_info':    'http://cdn.content.easports.com/fifa/fltOnlineAssets/C74DDF38-0B11-49b0-B199-2E2A11D1CC13/2014/fut/items/web/',
-    'keepalive':    'http://www.easports.com/fifa/football-club/keepalive',
 })
