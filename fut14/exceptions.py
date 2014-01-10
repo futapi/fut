c1ef9abd98ffcbb81474698335422758df831d59
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2014-01-10 11:37:36 +0100

    add few exceptions

--- fut14/exceptions.py
+++ fut14/exceptions.py
@@ -12,3 +12,14 @@ This module contains the set of fut14's exceptions.
 class Fut14Error(RuntimeError):
     """There was an ambiguous exception that occurred while handling
     your request."""
+
+class UnknownError(Fut14Error):
+    """Unknown error, please report full log at
+    https://github.com/oczkers/fut14/issues"""
+
+class ExpiredSession(Fut14Error):
+    """Session has expired,
+    you should send at least one request every ~10 minutes."""
+
+class InternalServerError(Fut14Error):
+    """Internal Server Error (ut)."""
