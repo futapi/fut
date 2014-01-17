3f6922f097cbfc2f9de33bad319a49d879bffb8a
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2014-01-17 21:04:02 +0100

    move logging code from __init__ to log

--- fut14/__init__.py
+++ fut14/__init__.py
@@ -25,21 +25,5 @@ __license__ = 'GNU GPL v3'
 __copyright__ = 'Copyright 2013 Piotr Staroszczyk'
 
 
-# logging
-import logging
-log_file_path = 'fut14.log'  # TODO: define logpath
-open(log_file_path, 'w').write('')  # remove old logs
-
-logformat = '%(asctime)s [%(levelname)s] [%(name)s] %(funcName)s: %(message)s (line %(lineno)d)'
-
-logger = logging.getLogger()
-#logger.setLevel(logging.DEBUG)
-#logger.setLevel(logging.INFO)
-logger.setLevel(logging.WARNING)
-logger_handler = logging.FileHandler(log_file_path)
-logger_handler.setFormatter(logging.Formatter(logformat))
-logger.addHandler(logger_handler)
-
-
 #from .api import baseId, cardInfo
 from .core import Core
