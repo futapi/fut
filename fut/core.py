628c0c2b9706f86fcf0b617f5b1a528819983395
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2014-10-21 23:06:29 +0200

    core: fix logging in __sendToPile__

--- fut/core.py
+++ fut/core.py
@@ -355,9 +355,9 @@ class Core(object):
 
         rc = self.__put__(self.urls['fut']['Item'], data=json.dumps(data))
         if rc['itemData'][0]['success']:
-            logger.info("{0} (itemId: {1}) moved to {2} Pile".format(trade_id, item_id, pile))
+            self.logger.info("{0} (itemId: {1}) moved to {2} Pile".format(trade_id, item_id, pile))
         else:
-            logger.error("{0} (itemId: {1}) NOT MOVED to {2} Pile. REASON: {3}".format(trade_id, item_id, pile, rc['itemData'][0]['reason']))
+            self.logger.error("{0} (itemId: {1}) NOT MOVED to {2} Pile. REASON: {3}".format(trade_id, item_id, pile, rc['itemData'][0]['reason']))
         return rc['itemData'][0]['success']
 
     def saveSession(self):
