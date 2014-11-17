1f021c2c9aeaf2ebbac7633d4fd3ec9d45162823
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2014-11-17 21:19:02 +0100

    core: add human readable coments

--- fut/core.py
+++ fut/core.py
@@ -34,14 +34,14 @@ def baseId(resource_id, return_version=False):
     """Calculates base id and version from a resource id."""
     version = 0
 
-    while resource_id > 0x01000000:
+    while resource_id > 0x01000000:  # 16777216
         version += 1
         if version == 1:
-            resource_id -= 0x70000000
+            resource_id -= 0x70000000  # 1879048192
         elif version == 2:
-            resource_id -= 0x03000000
+            resource_id -= 0x03000000  # 50331648
         else:
-            resource_id -= 0x01000000
+            resource_id -= 0x01000000  # 16777216
 
     if return_version:
         return resource_id, version
