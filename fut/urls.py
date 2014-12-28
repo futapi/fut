1e85d2e3081e32a3f95c810448aef2930fa862c9
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2014-12-28 23:48:04 +0100

    urls: enable ssl for all urls

--- fut/urls.py
+++ fut/urls.py
@@ -12,8 +12,7 @@ def __updateUrls__(urls, cl):
         rc = xmltodict.parse(requests.get(urls['fut_config']).content)
         services = rc['main']['services']['prod']
         path = '{0}{1}game/fifa/'.format(urls['fut_host'], rc['main']['directHttpServiceDestination'])
-        path_auth = '{0}/iframe/fut15{1}'.format(urls['main_site'].replace('https', 'http'),  # it's not working with ssl...
-                                                 rc['main']['httpServiceDestination'])
+        path_auth = '{0}/iframe/fut15{1}'.format(urls['main_site'], rc['main']['httpServiceDestination'])
         for i in services:
             if i == 'authentication':
                 urls['fut'][i] = path_auth+services[i]
@@ -28,13 +27,13 @@ def urls(platform, cl=None):
     urls = {
         'main_site':             'https://www.easports.com',
         'futweb':                'https://www.easports.com/iframe/fut15/?baseShowoffUrl=https%3A%2F%2Fwww.easports.com%2Ffifa%2Fultimate-team%2Fweb-app%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Ffifa%2Fultimate-team%2Fweb-app&locale=en_US',
-        'fut_config':            'http://www.easports.com/iframe/fut15/bundles/futweb/web/flash/xml/site_config.xml',  # add timestamp
-        'fut_home':              'http://www.easports.com/uk/fifa/football-club/ultimate-team',
+        'fut_config':            'https://www.easports.com/iframe/fut15/bundles/futweb/web/flash/xml/site_config.xml',  # add timestamp
+        'fut_home':              'https://www.easports.com/uk/fifa/football-club/ultimate-team',
         'fut':                   {},  # it's updated dynamicly (based on fut_config)
-        'fut_question':          'http://www.easports.com/iframe/fut15/p/ut/game/fifa15/phishing/question',  # add timestamp
-        'fut_validate':          'http://www.easports.com/iframe/fut15/p/ut/game/fifa15/phishing/validate',
-        'fut_captcha_img':       'http://www.easports.com/iframe/fut15/p/ut/captcha/img',  # add timestamp
-        'fut_captcha_validate':  'http://www.easports.com/iframe/fut15/p/ut/captcha/validate',
+        'fut_question':          'https://www.easports.com/iframe/fut15/p/ut/game/fifa15/phishing/question',  # add timestamp
+        'fut_validate':          'https://www.easports.com/iframe/fut15/p/ut/game/fifa15/phishing/validate',
+        'fut_captcha_img':       'https://www.easports.com/iframe/fut15/p/ut/captcha/img',  # add timestamp
+        'fut_captcha_validate':  'https://www.easports.com/iframe/fut15/p/ut/captcha/validate',
 
         'fut_host':              {'pc':   'https://utas.s2.fut.ea.com:443',
                                   'ps3':  'https://utas.s2.fut.ea.com:443',
@@ -43,8 +42,8 @@ def urls(platform, cl=None):
                                   'and':  'https://utas.fut.ea.com:443',
                                  },
 
-        'shards':                'http://www.easports.com/iframe/fut15/p/ut/shards',  # add timestamp
-        'acc_info':              'http://www.easports.com/iframe/fut15/p/ut/game/fifa15/user/accountinfo',
+        'shards':                'https://www.easports.com/iframe/fut15/p/ut/shards',  # add timestamp
+        'acc_info':              'https://www.easports.com/iframe/fut15/p/ut/game/fifa15/user/accountinfo',
         'card_info':             'https://fifa15.content.easports.com/fifa/fltOnlineAssets/8D941B48-51BB-4B87-960A-06A61A62EBC0/2015/fut/items/web/',
     }
     #urls['login'] = requests.get(urls['fut_home']).url
