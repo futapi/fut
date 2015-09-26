b66b21ea2c5083c3fddd2e3f0685a6f55cae0fa1
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2015-09-26 18:37:26 +0200

    fix #145 (baseId calculation) #140

--- fut/core.py
+++ fut/core.py
@@ -33,20 +33,21 @@ from .EAHashingAlgorithm import EAHashingAlgorithm
 def baseId(resource_id, return_version=False):
     """Calculates base id and version from a resource id."""
     version = 0
+    resource_id = abs(resource_id)
 
     while resource_id > 0x01000000:  # 16777216
         version += 1
         if version == 1:
-            resource_id -= 0x70000000  # 1879048192
+            resource_id -= 0x80000000  # 2147483648
         elif version == 2:
             resource_id -= 0x03000000  # 50331648
         else:
             resource_id -= 0x01000000  # 16777216
 
     if return_version:
-        return resource_id, version
+        return abs(resource_id), version
 
-    return resource_id
+    return abs(resource_id)
 
 
 def itemParse(item_data):
