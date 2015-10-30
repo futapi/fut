64fd2ad47f72201073277019409b4bb4fea7c49b
Jason Hunter <hunterjm@gmail.com>
2015-10-29 21:24:39 -0400

    Change class name to DelayedCore

--- fut/delayedcore.py
+++ fut/delayedcore.py
@@ -2,12 +2,12 @@ from .core import Core, cookies_file
 import random
 from time import time, sleep
 
-class Delayed(Core):
+class DelayedCore(Core):
     def __init__(self, email, passwd, secret_answer, platform='pc', code=None, emulate=None, debug=False, cookies=cookies_file):
         # Set initial delay
         self.delayInterval = 1.3
         self.delay = time()
-        super(Delayed, self).__init__(email, passwd, secret_answer, platform, code, emulate, debug, cookies)
+        super(DelayedCore, self).__init__(email, passwd, secret_answer, platform, code, emulate, debug, cookies)
 
     def setRequestDelay(self, delay):
         self.delayInterval = delay
@@ -18,12 +18,12 @@ class Delayed(Core):
         if self.delay > time():
             sleep(self.delay - time())
         self.delay = time() + (self.delayInterval * random.uniform(0.75, 2))
-        return super(Delayed, self).__request__(method, url, *args, **kwargs)
+        return super(DelayedCore, self).__request__(method, url, *args, **kwargs)
 
     def bid(self, trade_id, bid):
         # no delay between getting trade info and bidding
         delayInterval = self.delayInterval
         self.delayInterval = 0
-        result = super(Delayed, self).bid(trade_id, bid)
+        result = super(DelayedCore, self).bid(trade_id, bid)
         self.delayInterval = delayInterval
         return result
