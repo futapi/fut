0be239b00394c569d4ca52dbb614967384d595f2
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-04-14 09:11:16 +0000

    setup: enable classifiers

--- setup.py
+++ setup.py
@@ -44,28 +44,29 @@ setup(
     install_requires=requires,
     # license=open('LICENSE').read(),
     license=__license__,
-    # classifiers=(
-    #     'Development Status :: 3 - Alpha',
-    #     # 'Development Status :: 4 - Beta',
-    #     'Intended Audience :: Developers',
-    #     'Natural Language :: English',
-    #     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-    #     'Programming Language :: Python',
-    #     'Programming Language :: Python :: 2.6',
-    #     'Programming Language :: Python :: 2.7',
-    #     'Programming Language :: Python :: 3',
-    #     # 'Programming Language :: Python :: 3.0',  # not tested
-    #     # 'Programming Language :: Python :: 3.1',  # not tested
-    #     # 'Programming Language :: Python :: 3.2',  # dropped due requests incomapitibility
-    #     'Programming Language :: Python :: 3.3',
-    #     'Programming Language :: Python :: 3.4',
-    #     'Programming Language :: Python :: 3.5',
-    #     # 'Programming Language :: Python :: Implementation :: CPython',  # not tested
-    #     # 'Programming Language :: Python :: Implementation :: IronPython',  # not tested
-    #     'Programming Language :: Python :: Implementation :: PyPy',
-    #     'Topic :: Internet :: WWW/HTTP :: Site Management',
-    #     'Topic :: Software Development :: Libraries :: Python Modules',
-    # ),
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.0',  # not tested
+        # 'Programming Language :: Python :: 3.1',  # not tested
+        # 'Programming Language :: Python :: 3.2',  # dropped due requests incomapitibility
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        # 'Programming Language :: Python :: Implementation :: IronPython',  # not tested
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
     # entry_points={
     #     'console_scripts': [
     #         'fut = fut.cli:main',
