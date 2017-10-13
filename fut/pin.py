c967294958e25abde17f60d6bbe3525ef6b15081
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-10-13 16:41:57 +0200

    make options request before pinging #287

--- fut/pin.py
+++ fut/pin.py
@@ -104,6 +104,7 @@ class Pin(object):
                 "custom": self.custom,
                 "events": events}
         # print(data)  # DEBUG
+        self.r.options(pin_url)
         rc = self.r.post(pin_url, data=json.dumps(data)).json()
         if rc['status'] != 'ok':
             raise FutError('PinEvent is NOT OK, probably they changed something.')
