e1dfddcf788c7c39709eb341fe65e8ef4010f689
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-11-07 13:46:33 +0100

    sendToSbs: return False if item already in sbs

--- fut/core.py
+++ fut/core.py
@@ -1179,16 +1179,18 @@ class Core(object):
         return self.__request__(method, url, data=json.dumps(data))
 
     def sendToSbs(self, challenge_id, item_id):
-        """Send card to first free slot in sbs squad."""
+        """Send card FROM CLUB to first free slot in sbs squad."""
         # TODO?: multiple item_ids
         method = 'PUT'
         url = 'sbs/challenge/%s/squad' % challenge_id
 
-        moved = False
-        n = 0
         squad = self.sbsSquad(challenge_id)
         players = []
+        moved = False
+        n = 0
         for i in squad['squad']['players']:
+            if i['itemData']['id'] == item_id:  # item already in sbs  # TODO?: report reason
+                return False
             if i['itemData']['id'] == 0 and not moved:
                 i['itemData']['id'] = item_id
                 moved = True
