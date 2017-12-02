98904a0d0c7789108e5f883afc17f054e7aa1ccd
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-12-02 21:33:05 +0100

    fix #351 add missing params to club method

--- fut/core.py
+++ fut/core.py
@@ -1001,14 +1001,43 @@ class Core(object):
         else:
             return False
 
-    def club(self, sort='desc', ctype='player', defId='', start=0, count=91, level=None):
-        """Return items in your club, excluding consumables."""
+    def club(self, sort='desc', ctype='player', defId='', start=0, count=91,
+             level=None, category=None, assetId=None, league=None, club=None,
+             position=None, zone=None, nationality=None, rare=False, playStyle=None):
+        """Return items in your club, excluding consumables.
+
+        :param ctype: [development / ? / ?] Card type.
+        :param level: (optional) [?/?/gold] Card level.
+        :param category: (optional) [fitness/?/?] Card category.
+        :param assetId: (optional) Asset id.
+        :param defId: (optional) Definition id.
+        :param min_price: (optional) Minimal price.
+        :param max_price: (optional) Maximum price.
+        :param min_buy: (optional) Minimal buy now price.
+        :param max_buy: (optional) Maximum buy now price.
+        :param league: (optional) League id.
+        :param club: (optional) Club id.
+        :param position: (optional) Position.
+        :param nationality: (optional) Nation id.
+        :param rare: (optional) [boolean] True for searching special cards.
+        :param playStyle: (optional) Play style.
+        :param start: (optional) Start page sent to server so it supposed to be 12/15, 24/30 etc. (default platform page_size*n)
+        :param page_size: (optional) Page size (items per page)
+        """
         method = 'GET'
         url = 'club'
 
         params = {'sort': sort, 'type': ctype, 'defId': defId, 'start': start, 'count': count}
-        if level:
-            params['level'] = level
+        if level:       params['lev'] = level
+        if category:    params['cat'] = category
+        if assetId:     params['maskedDefId'] = assetId
+        if league:      params['leag'] = league
+        if club:        params['team'] = club
+        if position:    params['pos'] = position
+        if zone:        params['zone'] = zone
+        if nationality: params['nat'] = nationality
+        if rare:        params['rare'] = 'SP'
+        if playStyle:   params['playStyle'] = playStyle
         rc = self.__request__(method, url, params=params)
 
         # pinEvent
