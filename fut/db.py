c287dd0096384da5e202444a23c99e039afd6d14
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2018-01-09 17:38:57 +0100

    init db

---
+++ fut/db.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+fut.db
+~~~~~~~~~~~~~~~~~~~~~
+
+This module implements the fut's method to access our database.
+
+"""
+
+# This is just prototype, database is not created yet.
+
+import requests
