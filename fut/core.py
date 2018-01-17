87a0d6c63d0eacb7f23ba81b199b51f83a774448
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2018-01-17 12:40:35 +0100

    logout when captcha raised

--- fut/core.py
+++ fut/core.py
@@ -730,6 +730,7 @@ class Core(object):
                 # pinEvents
                 events = [self.pin.event('error')]
                 self.pin.send(events)
+                self.logout()
                 raise Captcha()
             elif rc.status_code == 460:
                 raise PermissionDenied(460)
@@ -794,7 +795,11 @@ class Core(object):
 
         :params save: False if You don't want to save cookies.
         """
-        self.r.delete('https://%s/ut/auth' % self.fut_host, timeout=self.timeout)
+        self.r.get('https://www.easports.com/signout', params={'ct': self._})
+        self.r.get('https://accounts.ea.com/connect/clearsid', params={'ct': self._})
+        self.r.get('https://beta.www.origin.com/views/logout.html', params={'ct': self._})
+        self.r.get('https://help.ea.com/community/logout/', params={'ct': self._})
+        # self.r.delete('https://%s/ut/auth' % self.fut_host, timeout=self.timeout)
         if save:
             self.saveSession()
         # needed? https://accounts.ea.com/connect/logout?client_id=FIFA-18-WEBCLIENT&redirect_uri=https://www.easports.com/fifa/ultimate-team/web-app/auth.html
