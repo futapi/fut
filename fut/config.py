729ac2a759958bdd5150a8bfb6571164ac5b0dba
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2018-10-07 22:22:06 +0200

    bump chrome verison

--- fut/config.py
+++ fut/config.py
@@ -1,6 +1,6 @@
 # chrome 58 @ win10
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip,deflate,sdch, br',
     'Accept-Language': 'en-US,en;q=0.8',
